83d473e89a2c14d2834c64b843f0f185576993d6
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-04-21 11:35:57 +0530

    add book and bookauthor model

--- backend/models.py
+++ backend/models.py
@@ -82,3 +82,32 @@ class AdminUser(CustomUser):
         proxy = True
         verbose_name = 'Admin'
         verbose_name_plural = 'Admins'
+
+class Book(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    title = models.CharField(max_length=255)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    publication_date = models.DateField()
+
+    copies_owned = models.IntegerField()
+
+    authors = models.ManyToManyField('CustomUser', through='BookAuthor')
+
+    cover_image = models.ImageField(upload_to='cover_image', blank=True, null=True, default='no_image_available.jpg')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "book"
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'book_author'
+        unique_together = ('book', 'author')  # prevents duplicate author entries for a book
