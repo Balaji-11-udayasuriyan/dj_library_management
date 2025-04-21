78006e1dc54942d248b6ed252f8fa7241dc3e7df
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-04-21 11:36:13 +0530

    add book and book author in  admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Category,AdminUser,AuthorUser,MemberUser
-from forms import CustomUserChangeForm,CustomUserCreationForm
+from .models import Category,AdminUser,AuthorUser,MemberUser,CustomUser,BookAuthor,Book
+from backend.forms import CustomUserChangeForm,CustomUserCreationForm
 from django.db.models import Q
+
+from django.contrib.auth.admin import UserAdmin
+
 
 class BaseCustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -52,3 +55,35 @@ class AdminUserAdmin(BaseCustomUserAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
+
+class BookAuthorInline(admin.TabularInline):
+    model = BookAuthor
+    extra = 1  # Show 1 empty row to add new relationships
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == 'author':
+            # Filter only users in the "Author" group
+            kwargs["queryset"] = CustomUser.objects.filter(groups__name='Author')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    inlines = [BookAuthorInline]
+
+    list_display = ('title', 'category', 'publication_date', 'copies_owned', 'image_tag',)
+
+    search_fields = ('title',)
+
+    list_filter = ('category', 'publication_date')
+
+    def image_tag(self, obj):
+        return format_html('<img src = "{}" width = "150" height="150" />'.format(obj.cover_image.url))
+
+    image_tag.short_description = 'Image'
+
+
+# Utility function to filter only members
+def get_member_queryset():
+    return CustomUser.objects.filter(groups__name='Member')
+
