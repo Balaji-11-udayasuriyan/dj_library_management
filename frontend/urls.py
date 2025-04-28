33f90ee7ecee1d38db57e38bc5486a1b5b0370b0
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-04-28 11:35:13 +0530

    add dashboard logic

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,11 @@
+from tkinter.font import names
+
 from django.urls import path
 from . import views
-from .views import home
+from .views import home, member_register
 
 urlpatterns = [
-    path('', views.home, name ='home')
+    path('', views.home, name ='home'),
+    path('register/', views.member_register, name ='register'),
+    path('dashboard/', views.dashboard, name = 'dashboard')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from frontend.forms import RegisterForm
+
 
 # Create your views here.
 def home(request):
     return render(request, "frontend/home.html")
+
+# Member Registration
+def member_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = RegisterForm()
+    return render(request, 'frontend/auth/register.html', {'form': form})
+
+def dashboard(request):
+    return render(request, "frontend/auth/dashboard.html")
